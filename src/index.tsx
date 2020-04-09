8ffb884bbd3b075be59569282eb1fee7967bb6e2
SkyLightQP <combbm@naver.com>
2020-04-09 19:37:36 +0900

    Create Home page and NavBar component
    Add styled-components and tailwindcss dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Home from './pages/Home';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import 'tailwindcss/base.css';
+import 'tailwindcss/components.css.css';
+import 'tailwindcss/utilities.css.css';
+
+ReactDOM.render(<Home />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
