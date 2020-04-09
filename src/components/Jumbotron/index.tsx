59c01569b3979b3b87fd58f724439cb9af48d6bf
SkyLightQP <combbm@naver.com>
2020-04-09 22:07:08 +0900

    Create Jumbotron component
    Add background asset

---
+++ src/components/Jumbotron/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+import background from '../../assets/images/background.jpg';
+
+const JumbotronStyle = styled.div`
+  background-image: url(${background});
+  min-height: calc(100vh - 4em);
+  min-width: 100%;
+  background-repeat: no-repeat;
+  background-size : cover;
+`;
+
+const Jumbotron: React.FC = () => {
+  return (
+    <JumbotronStyle className="flex bg-blue-500 min-h-full justify-center items-center">
+      <p className="font-bold text-5xl text-center text-white">멋져보이는 말</p>
+    </JumbotronStyle>
+  );
+};
+
+export default Jumbotron;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import DefaultLayout from '../layouts/DefaultLayout';
+import Jumbotron from '../components/Jumbotron';
 
 const Home: React.FC = () => {
   return (
     <DefaultLayout>
-      <p>Hello World</p>
+      <Jumbotron />
     </DefaultLayout>
   );
 };
