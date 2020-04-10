b87bb8757591d855d0c13f73abc2559da9718379
Coder-Iro <doohee006@gmail.com>
2020-04-11 05:59:52 +0900

    Edited catchphrase

--- src/components/AboutAs/index.tsx
+++ src/components/AboutAs/index.tsx
@@ -6,7 +6,7 @@ import background from '../../assets/images/background.jpg';
 const Jumbotron: React.FC = () => {
   return (
     <div className="flex min-h-full justify-center items-center pt-8">
-      <p className="font-bold text-5xl text-center text-white">멋져보이는 말</p>
+      <p className="font-bold text-5xl text-center text-white">We Make 'IF' to 'Reality', 'Dream' to 'Action'</p>
     </div>
   );
 };
