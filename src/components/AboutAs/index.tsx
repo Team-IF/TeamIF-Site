5d4e27cceb0ba48847b133c0da2f193a2e679fc1
SkyLightQP <combbm@naver.com>
2020-04-11 17:04:05 +0900

    Fix content of AboutAs component

--- src/components/AboutAs/index.tsx
+++ src/components/AboutAs/index.tsx
@@ -1,15 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
-import background from '../../assets/images/background.jpg';
 
-
-const Jumbotron: React.FC = () => {
-  return (
-    <div className="flex min-h-full justify-center items-center pt-8">
-      <p className="font-bold text-5xl text-center text-white">We Make 'IF' to 'Reality', 'Dream' to 'Action'</p>
-    </div>
-  );
+const AboutAs: React.FC = () => {
+  return <></>;
 };
 
-export default Jumbotron;
-
+export default AboutAs;
