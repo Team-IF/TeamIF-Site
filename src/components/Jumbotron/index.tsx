43118f1aa0a91150878f60cd31896b1c610e3a09
SkyLightQP <combbm@naver.com>
2020-04-11 17:07:46 +0900

    Fix catchphrase design in Jumbotron

--- src/components/Jumbotron/index.tsx
+++ src/components/Jumbotron/index.tsx
@@ -2,19 +2,22 @@ import React from 'react';
 import styled from 'styled-components';
 import background from '../../assets/images/background.jpg';
 
-const JumbotronStyle = styled.div`
+const JumbotronBackgroundStyle = styled.div`
   background-image: url(${background});
-  min-height: calc(100vh - 4em);
+  min-height: calc(100vh - 4.9em);
   min-width: 100%;
   background-repeat: no-repeat;
-  background-size : cover;
+  background-size: cover;
 `;
 
 const Jumbotron: React.FC = () => {
   return (
-    <JumbotronStyle className="flex bg-blue-500 min-h-full justify-center items-center bg-fixed">
-      <p className="font-bold text-5xl text-center text-white">멋져보이는 말</p>
-    </JumbotronStyle>
+    <JumbotronBackgroundStyle className="flex bg-blue-500 min-h-full justify-center items-center bg-fixed">
+      <p className="text-5xl text-center text-white">
+        We Make <span className="font-bold">IF</span> to <span className="font-bold">Reality</span>,{' '}
+        <span className="font-bold">Dream</span> to <span className="font-bold">Action</span>
+      </p>
+    </JumbotronBackgroundStyle>
   );
 };
 
