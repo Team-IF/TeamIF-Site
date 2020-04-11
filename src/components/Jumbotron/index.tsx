d3cb3d2622fa7a33b802e5ee9adf96827d20cf5f
SkyLightQP <combbm@naver.com>
2020-04-11 18:08:03 +0900

    Remove blur effect in css
    Add blur effect in jpg image

--- src/components/Jumbotron/index.tsx
+++ src/components/Jumbotron/index.tsx
@@ -3,27 +3,25 @@ import styled from 'styled-components';
 import background from '../../assets/images/background.jpg';
 
 const JumbotronContainer = styled.div`
-  min-height: calc(100vh - 4.9em);
-  min-width: 100%;
+  height: calc(100vh - 4.9em);
+  width: 100vw;
 `;
 
 const BackgroundStyle = styled.div`
-  position: fixed;
+  position: absolute;
   top: 0;
   left: 0;
-  width: 100%;
-  height: 100%;
+  min-height: 100%;
+  min-width: 100%;
   z-index: -1;
   background-image: url(${background});
   background-repeat: no-repeat;
   background-size: cover;
-  filter: blur(3px);
-  transform: scale(1.02);
 `;
 
 const Jumbotron: React.FC = () => {
   return (
-    <JumbotronContainer className="flex min-h-full justify-center items-center bg-fixed">
+    <JumbotronContainer className="flex justify-center items-center bg-fixed">
       <p className="text-5xl text-center text-white">
         We Make <span className="font-bold">IF</span> to <span className="font-bold">Reality</span>,{' '}
         <span className="font-bold">Dream</span> to <span className="font-bold">Action</span>
