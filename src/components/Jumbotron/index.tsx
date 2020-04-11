22f9221b5479786ccfcd0800b0d7a7e1f6c67c89
SkyLightQP <combbm@naver.com>
2020-04-11 17:21:36 +0900

    Change jumbotron background image
    Apply blur effect in jumbotron background

--- src/components/Jumbotron/index.tsx
+++ src/components/Jumbotron/index.tsx
@@ -2,22 +2,34 @@ import React from 'react';
 import styled from 'styled-components';
 import background from '../../assets/images/background.jpg';
 
-const JumbotronBackgroundStyle = styled.div`
-  background-image: url(${background});
+const JumbotronContainer = styled.div`
   min-height: calc(100vh - 4.9em);
   min-width: 100%;
+`;
+
+const BackgroundStyle = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  background-image: url(${background});
   background-repeat: no-repeat;
   background-size: cover;
+  filter: blur(3px);
+  transform: scale(1.02);
 `;
 
 const Jumbotron: React.FC = () => {
   return (
-    <JumbotronBackgroundStyle className="flex bg-blue-500 min-h-full justify-center items-center bg-fixed">
+    <JumbotronContainer className="flex min-h-full justify-center items-center bg-fixed">
       <p className="text-5xl text-center text-white">
         We Make <span className="font-bold">IF</span> to <span className="font-bold">Reality</span>,{' '}
         <span className="font-bold">Dream</span> to <span className="font-bold">Action</span>
       </p>
-    </JumbotronBackgroundStyle>
+      <BackgroundStyle />
+    </JumbotronContainer>
   );
 };
 
