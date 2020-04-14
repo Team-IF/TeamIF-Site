11db01f603ca85d7233386ca79e6fffd5aa66cdc
SkyLightQP <combbm@naver.com>
2020-04-14 13:29:50 +0900

    Fix margin and blank of icon
    Add alt attribute in icon img tag
    Remove TextStyle
    Update README.md

--- README.md
+++ README.md
@@ -8,3 +8,5 @@
     - 사이트 빌드
 - `yarn run start`
     - 개발 모드 시작
+- `yarn run lint`
+    - eslint 검사
--- src/components/AboutUs/index.tsx
+++ src/components/AboutUs/index.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 import Section from '../../atomics/Section';
 import Panel from './Panel';
 import PanelHeader from '../../atomics/AboutUs/PanelHeader';
 import PanelBody from '../../atomics/AboutUs/PanelBody';
-import styled from 'styled-components';
-import iconBalloon from '../../assets/images/icon/Balloon.png'
-import iconPictureFrame from '../../assets/images/icon/PictureFrame.png'
-import iconGear from '../../assets/images/icon/Gear.png'
+import iconBalloon from '../../assets/images/icon/Balloon.png';
+import iconPictureFrame from '../../assets/images/icon/PictureFrame.png';
+import iconGear from '../../assets/images/icon/Gear.png';
 
 const IconStyle = styled.img`
   height: 40px;
   width: 40px;
   float: left;
-  vertical-align: middle
-`;
 
-const TextStyle = styled.p`
-  font-size: 32px;
-  float: right;
+  margin-top: 4px;
 `;
 
 const AboutUs: React.FC = () => {
@@ -32,30 +28,24 @@ const AboutUs: React.FC = () => {
         <div className="flex lg:flex-row flex-col justify-center items-center mt-16">
           <Panel>
             <PanelHeader>
-              <span role="img" aria-label="Balloon">
-                <IconStyle src={iconBalloon} />
-              </span>{' '}
-              <TextStyle>&nbsp;우리는 꿈을 꾸며</TextStyle>
+              <IconStyle src={iconBalloon} alt="Balloon" />
+              우리는 꿈을 꾸며
             </PanelHeader>
             <PanelBody>가나다라마바사</PanelBody>
           </Panel>
 
           <Panel>
             <PanelHeader>
-              <span role="img" aria-label="PictureFrame">
-                <IconStyle src={iconPictureFrame} />
-              </span>{' '}
-              <TextStyle>&nbsp;상상을 이루고</TextStyle>
+              <IconStyle src={iconPictureFrame} alt="PictureFrame" />
+              &nbsp;상상을 이루고
             </PanelHeader>
             <PanelBody>가나다라마바사</PanelBody>
           </Panel>
 
           <Panel>
             <PanelHeader>
-              <span role="img" aria-label="Gear">
-                <IconStyle src={iconGear} />
-              </span>{' '}
-              <TextStyle>&nbsp;현실로 만듭니다</TextStyle>
+              <IconStyle src={iconGear} alt="Gear" />
+              &nbsp;현실로 만듭니다
             </PanelHeader>
             <PanelBody>가나다라마바사</PanelBody>
           </Panel>
