8b647ddd0a0a756ec91366ab6e46e31469304e63
SkyLightQP <combbm@naver.com>
2020-04-18 18:24:20 +0900

    Remove navigation bar
    Fix scrollingSpeed option

--- src/atomics/NavBar/NavBarHeader/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NavBarHeader: React.FC = ({ children }) => {
-  return <div className="flex items-center flex-shrink-0 text-white mr-8 lg:ml-8 ml-2">{children}</div>;
-};
-
-export default NavBarHeader;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,49 +1,31 @@
 import React from 'react';
 // @ts-ignore
 import ReactFullpage from '@fullpage/react-fullpage';
-// import DefaultLayout from '../layouts/DefaultLayout';
 import Jumbotron from '../components/Jumbotron';
 import AboutUs from '../components/AboutUs';
 import Members from '../components/Members';
-import NavBar from '../components/NavBar';
 
 const Home: React.FC = () => {
   return (
-    <>
-      <NavBar/>
-      <ReactFullpage
-        licensekey="OPEN-SOURCE-GPLV3-LICENSE"
-        scrollingSpeed={1000}
-        render={() => {
-          return (
-            <ReactFullpage.Wrapper>
-              <div className="section">
-                <Jumbotron/>
-              </div>
-              <div className="section">
-                <AboutUs/>
-              </div>
-              <div className="section">
-                <Members/>
-              </div>
-            </ReactFullpage.Wrapper>
-          );
-        }}
-      />
-    </>
-    /* <DefaultLayout>
-      <ScrollContainer>
-        <ScrollWrapper>
-          <Jumbotron />
-        </ScrollWrapper>
-        <ScrollWrapper>
-          <AboutUs />
-        </ScrollWrapper>
-        <ScrollWrapper>
-          <Members />
-        </ScrollWrapper>
-      </ScrollContainer>
-    </DefaultLayout> */
+    <ReactFullpage
+      licensekey="OPEN-SOURCE-GPLV3-LICENSE"
+      scrollingSpeed={800}
+      render={() => {
+        return (
+          <ReactFullpage.Wrapper>
+            <div className="section">
+              <Jumbotron />
+            </div>
+            <div className="section">
+              <AboutUs />
+            </div>
+            <div className="section">
+              <Members />
+            </div>
+          </ReactFullpage.Wrapper>
+        );
+      }}
+    />
   );
 };
 
