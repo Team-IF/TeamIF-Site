2682506b4c589e7cbb3bee4fa5598142ae0feba2
SkyLightQP <combbm@naver.com>
2020-04-23 20:25:52 +0900

    Add profile images

--- src/components/Members/index.tsx
+++ src/components/Members/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
-import Section from '../../atomics/Section';
 import styled from 'styled-components';
+import Section from '../../atomics/Section';
+import background from '../../assets/images/background/backgroundFlow.jpg';
+import Circle from './Circle';
+
+interface ProfileData {
+  readonly name: string;
+  readonly description: string;
+  readonly image: string;
+}
 
 const SectionStyle = styled(Section)`
   display: flex;
@@ -18,10 +26,51 @@ const TitleStyle = styled.p`
   }
 `;
 
+const ListWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CircleListStyle = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 70%;
+  margin-top: 2rem;
+  justify-content: center;
+`;
+
 const Members: React.FC = () => {
+  const profiles: ProfileData[] = [
+    {
+      name: '이로',
+      description: 'ㅁㅇㄴ',
+      image: require('../../assets/images/profile/1.png')
+    }
+  ];
+
   return (
-    <SectionStyle id="members">
-      <TitleStyle>팀원 소개</TitleStyle>
+    <SectionStyle>
+      <div>
+        <TitleStyle>팀원 소개</TitleStyle>
+
+        <ListWrapper>
+          <CircleListStyle>
+            {
+              profiles.map((item: ProfileData) => (
+                <Circle key={item.name} profile={item.image}>{item.description}</Circle>
+              ))
+            }
+
+            <Circle profile={background}>테스트</Circle>
+            <Circle profile={background}>테스트</Circle>
+            <Circle profile={background}>테스트</Circle>
+            <Circle profile={background}>테스트</Circle>
+            <Circle profile={background}>테스트</Circle>
+          </CircleListStyle>
+        </ListWrapper>
+      </div>
     </SectionStyle>
   );
 };
