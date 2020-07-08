490e1ab7b8737ea98596328316cfbebeec1ea88f
moole100 <phmi1621@naver.com>
2020-07-08 22:00:48 +0900

    Add members page & Update description of 'moole100'

--- src/stores/MemberData.ts
+++ src/stores/MemberData.ts
@@ -8,42 +8,42 @@ export const members: MemberData[] = [
   {
     name: '이로',
     description: '만사가 귀찮은 유사 프로그래머',
-    image: require('../assets/images/profile/1.png'),
+    image: require('../assets/images/profile/Iro.png'),
   },
   {
     name: 'gangjun06',
     description: '',
-    image: require('../assets/images/profile/2.png'),
+    image: require('../assets/images/profile/GangJun.png'),
   },
   {
     name: 'GPL_Developer',
     description: '개발에 찌든 고등학생',
-    image: require('../assets/images/profile/3.png'),
+    image: require('../assets/images/profile/MAGMA.png'),
   },
   {
     name: 'moole100',
-    description: '몰백이는 오랜지 병 으로 사망했다..',
-    image: require('../assets/images/profile/4.png'),
+    description: '아무도 내가 프로그래밍 한다는것을 믿지 않는다.',
+    image: require('../assets/images/profile/moole100.png'),
   },
   {
     name: 'Nrvnqsr Chaos',
     description: '열심히 개발하는 Team IF중 유일한 잉여',
-    image: require('../assets/images/profile/5.png'),
+    image: require('../assets/images/profile/NC.png'),
   },
   {
     name: 'PatrickKR',
     description: '이것저것 하는 Patrick입니다. 반갑습니다.',
-    image: require('../assets/images/profile/6.png'),
+    image: require('../assets/images/profile/PatrickKR.png'),
   },
   {
     name: 'Star_Pixeller',
     description: '',
-    image: require('../assets/images/profile/7.png'),
+    image: require('../assets/images/profile/SP.png'),
   },
   {
     name: '하늘빛',
     description: '코드 스타일과 패턴에 관심이 많아요',
-    image: require('../assets/images/profile/8.png'),
+    image: require('../assets/images/profile/SkyBim.png'),
   },
   {
     name: 'ksi123456ab',
@@ -53,23 +53,58 @@ export const members: MemberData[] = [
   {
     name: 'PotatoY',
     description: '잡다한 활동을 전문적으로 하는 사람입니다!',
-    image: require('../assets/images/profile/9.png'),
+    image: require('../assets/images/profile/Potato.png'),
   },
   {
     name: 'WieeRd',
     description: '',
-    image: require('../assets/images/profile/10.png'),
+    image: require('../assets/images/profile/WieeRd.png'),
   },
   {
     name: '라피스',
     description:
       '꿈꾸는 고등학생 프로그래머입니다!',
-    image: require('../assets/images/profile/11.png'),
+    image: require('../assets/images/profile/Lapis.png'),
   },
   {
     name: ' Nitro',
     description:
       '배움을 받고 배움을 공유하는 개발자가 되고 싶습니다.',
-    image: require('../assets/images/profile/12.png'),
+    image: require('../assets/images/profile/Nitro.png'),
+  },
+  {
+    name: '༺ৡۣۜ͜B̸1̷a̶c̵k̸A̸n̸g̶e̴1̸ৡۣۜ༻',
+    description: 'Developer',
+    image: require('../assets/images/profile/Black_Angel.png'),
+  },
+  {
+    name: 'F E Z',
+    description: '뇌절온 중2 개발자',
+    image: require('../assets/images/profile/FEZ.png'),
+  },
+  {
+    name: '러리',
+    description: '',
+    image: require('../assets/images/profile/doralife.png'),
+  },
+  {
+    name: 'Isomorphism',
+    description: '',
+    image: require('../assets/images/profile/Isomorphism.png'),
+  },
+  {
+    name: '키네틱',
+    description: '',
+    image: require('../assets/images/profile/Kinetic.png'),
+  },
+  {
+    name: '중딩 프로그래머',
+    description: '',
+    image: require('../assets/images/profile/default_profile.png'),
+  },
+  {
+    name: 'LEDAM',
+    description: '',
+    image: require('../assets/images/profile/rodine.png'),
   }
 ];
