d62f3d4895bc3ae8f5e56bb38f633e2d28b28e5a
moole100 <phmi1621@naver.com>
2020-08-23 20:16:13 +0900

    update email data

--- src/stores/MemberData.ts
+++ src/stores/MemberData.ts
@@ -10,105 +10,126 @@ export const members: MemberData[] = [
     name: '이로',
     description: '만사가 귀찮은 유사 프로그래머',
     image: require('../assets/images/profile/Iro.png'),
+    email: 'doohee006@gmail.com'
   },
   {
     name: 'gangjun06',
     description: '',
     image: require('../assets/images/profile/GangJun.png'),
+    email: 'gangjun2006@gmail.com'
   },
   {
     name: 'GPL_Developer',
     description: '개발에 찌든 고등학생',
     image: require('../assets/images/profile/MAGMA.png'),
+    email: 'gplnature@gmail.com'
   },
   {
     name: 'moole100',
     description: 'Return Python;',
     image: require('../assets/images/profile/moole100.png'),
+    email: 'phmi162193@gmail.com'
   },
   {
     name: 'Nrvnqsr Chaos',
     description: '열심히 개발하는 Team IF중 유일한 잉여',
     image: require('../assets/images/profile/NC.png'),
+    email: ''
   },
   {
     name: 'PatrickKR',
     description: '이것저것 하는 Patrick입니다. 반갑습니다.',
     image: require('../assets/images/profile/PatrickKR.png'),
+    email: 'mailpatrickkr@gmail.com'
   },
   {
     name: 'Star_Pixeller',
     description: '',
     image: require('../assets/images/profile/SP.png'),
+    email: 'starpixeller@gmail.com'
   },
   {
     name: '하늘빛',
     description: '코드 스타일과 패턴에 관심이 많아요',
     image: require('../assets/images/profile/SkyBim.png'),
+    email: 'combbm@gmail.com'
   },
   {
     name: 'ksi123456ab',
     description: '고3인데 공부는 안하고 컴퓨터만 하는 개발자입니다',
     image: require('../assets/images/profile/default_profile.png'),
+    email: 'ksi123456ab@naver.com'
   },
   {
     name: 'PotatoY',
     description: '잡다한 활동을 전문적으로 하는 사람입니다!',
     image: require('../assets/images/profile/Potato.png'),
+    email: 'jaewonyu.dev@gmail.com'
   },
   {
     name: 'WieeRd',
     description: '',
     image: require('../assets/images/profile/WieeRd.png'),
+    email: ''
   },
   {
     name: '라피스',
     description: '꾸준히 공부하고 성장하려 노력하는 학생 개발자입니다.',
-    image: require('../assets/images/profile/Lapis.png')
+    image: require('../assets/images/profile/Lapis.png'),
+    email: 'lapis0875@gmail.com'
   },
   {
-    name: ' Nitro',
+    name: 'Nitro',
     description: '배움을 받고 배움을 공유하는 개발자가 되고 싶습니다.',
-    image: require('../assets/images/profile/Nitro.png')
+    image: require('../assets/images/profile/Nitro.png'),
+    email: 'junpark0207@gmail.com'
   },
   {
     name: '༺ৡۣۜ͜B̸1̷a̶c̵k̸A̸n̸g̶e̴1̸ৡۣۜ༻',
     description: 'Developer',
     image: require('../assets/images/profile/Black_Angel.png'),
+    email: 'admin@b1ackange1.me'
   },
   {
     name: 'F E Z',
     description: '뇌절온 중2 개발자',
     image: require('../assets/images/profile/FEZ.png'),
+    email:'qnqn60360@gmail.com'
   },
   {
     name: '러리',
     description: '안녕하세요! 잡탕 러리입니다 :)  ',
     image: require('../assets/images/profile/doralife.png'),
+    email: 'doralife12@gmail.com'
   },
   {
     name: 'Tentacles',
     description: '수학조아',
     image: require('../assets/images/profile/Isomorphism.png'),
+    email: ''
   },
   {
     name: '키네틱',
     description: '마신 커피만큼 코드를 뽑아냅니다.',
     image: require('../assets/images/profile/Kinetic.png'),
+    email: 'aheui@kakao.com'
   },
   {
     name: '중딩 프로그래머',
     description: '웹 개발에 관심이 많은 중학생 프로그래머입니다!',
     image: require('../assets/images/profile/default_profile.png'),
+    email: ''
   },
   {
     name: 'LEDAM',
     description: '파이썬은 빵과 태양',
-    image: require('../assets/images/profile/rodine.png')
+    image: require('../assets/images/profile/rodine.png'),
+    email: ''
   },
   {
     name: '암드빠인 고양이',
     description: '프로그래밍에 관심이 많은 중학생 암고입니다',
-    image: require('../assets/images/profile/AMDCAT.png')
+    image: require('../assets/images/profile/AMDCAT.png'),
+    email: 'june2008a@gmail.com'
   }
 ];
