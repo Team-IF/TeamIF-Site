b84a8d7be8ed1d117b8823caaab0177cd645fa2f
combbm (via Travis CI) <combbm@naver.com>
2020-09-26 12:23:52 +0000

    Deploy teamif.io to github.com/Team-IF/TeamIF-Site.git:gh-pages

---
+++ precache-manifest.3509c009cab48cd55deb852fc42287ee.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37148ea4443e18d1d6f04a69113b70b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f93029e681abe7622d2",
+    "url": "/static/js/2.74f2eb9c.chunk.js"
+  },
+  {
+    "revision": "db43fa1d2eb03629a81d8c379ec33a02",
+    "url": "/static/js/2.74f2eb9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ae27f05dab7c75b317a",
+    "url": "/static/js/main.17fe8e32.chunk.js"
+  },
+  {
+    "revision": "472c9ef8c0494fa05128",
+    "url": "/static/js/runtime-main.c8cf082f.js"
+  },
+  {
+    "revision": "6d5e71f9e92dd6360b48a27be42901b3",
+    "url": "/static/media/Balloon.6d5e71f9.png"
+  },
+  {
+    "revision": "defb2531a22b05c66b4d4ae553122980",
+    "url": "/static/media/Gear.defb2531.png"
+  },
+  {
+    "revision": "e5783935530e59e21d47f3374c90f522",
+    "url": "/static/media/PictureFrame.e5783935.png"
+  },
+  {
+    "revision": "9dfca74117026be1444a13fc3e92747e",
+    "url": "/static/media/backgroundFlow.9dfca741.jpg"
+  },
+  {
+    "revision": "f4b008292fc6c3aee1af57df991418c0",
+    "url": "/static/media/logo.f4b00829.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.22cd4bfc2d0738bd85e2e011c19fb1fb.js"
+  "/precache-manifest.3509c009cab48cd55deb852fc42287ee.js"
 );
 
 self.addEventListener('message', (event) => {
