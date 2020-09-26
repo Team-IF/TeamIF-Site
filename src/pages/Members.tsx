5d8433968efa686d750db8e5889a2f7a335af716
combbm <combbm@naver.com>
2020-09-26 21:21:15 +0900

    Delete unused member images

--- src/pages/Members.tsx
+++ src/pages/Members.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
+import md5 from 'md5';
 import NavBar from '../components/NavBar';
 import Circle from '../components/Members/Circle';
 import { MemberData, members } from '../stores/MemberData';
 import SectionTitle from '../atomics/SectionTitle';
-import md5 from 'md5';
 
 const SectionStyle = styled.section`
   display: flex;
@@ -48,7 +48,11 @@ const Members: React.FC = () => {
               {members.map((item: MemberData) => (
                 <Circle
                   key={item.name}
-                  profile={'https://www.gravatar.com/avatar/' + md5(item.email) + '?s=1024&d=https%3A%2F%2Fteamif%2Eio%2Fdefault_profile%2Epng'}
+                  profile={
+                    `https://www.gravatar.com/avatar/${
+                      md5(item.email)
+                    }?s=1024&d=https%3A%2F%2Fteamif%2Eio%2Fdefault_profile%2Epng`
+                  }
                 >
                   <div>
                     <b>{item.name}</b>
--- src/stores/MemberData.ts
+++ src/stores/MemberData.ts
@@ -2,140 +2,117 @@ export interface MemberData {
   readonly email: string;
   readonly name: string;
   readonly description: string;
-  readonly image: string;
 }
 
 export const members: MemberData[] = [
   {
     name: '이로',
     description: '만사가 귀찮은 유사 프로그래머',
-    image: require('../assets/images/profile/Iro.png'),
-    email: 'doohee006@gmail.com'
+    email: 'doohee006@gmail.com',
   },
   {
     name: 'gangjun06',
     description: '',
-    image: require('../assets/images/profile/GangJun.png'),
-    email: 'gangjun2006@gmail.com'
+    email: 'gangjun2006@gmail.com',
   },
   {
     name: 'GPL_Developer',
     description: '개발에 찌든 고등학생',
-    image: require('../assets/images/profile/MAGMA.png'),
-    email: 'gplnature@gmail.com'
+    email: 'gplnature@gmail.com',
   },
   {
     name: 'moole100',
     description: 'Return Python;',
-    image: require('../assets/images/profile/moole100.png'),
-    email: 'phmi162193@gmail.com'
+    email: 'phmi162193@gmail.com',
   },
   {
     name: 'Nrvnqsr Chaos',
     description: '열심히 개발하는 Team IF중 유일한 잉여',
-    image: require('../assets/images/profile/NC.png'),
-    email: 'jamfongcha@gmail.com'
+    email: 'jamfongcha@gmail.com',
   },
   {
     name: 'PatrickKR',
     description: '이것저것 하는 Patrick입니다. 반갑습니다.',
-    image: require('../assets/images/profile/PatrickKR.png'),
-    email: 'mailpatrickkr@gmail.com'
+    email: 'mailpatrickkr@gmail.com',
   },
   {
     name: '청월이',
     description: '',
-    image: require('../assets/images/profile/CheongWol.png'),
-    email: 'cheong7wol@gmail.com'
+    email: 'cheong7wol@gmail.com',
   },
   {
     name: '하늘빛',
     description: '코드 스타일과 패턴에 관심이 많아요',
-    image: require('../assets/images/profile/SkyBim.png'),
-    email: 'skylightqp@teamif.io'
+    email: 'skylightqp@teamif.io',
   },
   {
     name: 'ksi123456ab',
     description: '고3인데 공부는 안하고 컴퓨터만 하는 개발자입니다',
-    image: require('../assets/images/profile/default_profile.png'),
-    email: 'ksi123456ab@naver.com'
+    email: 'ksi123456ab@naver.com',
   },
   {
     name: 'PotatoY',
     description: '잡다한 활동을 전문적으로 하는 사람입니다!',
-    image: require('../assets/images/profile/Potato.png'),
-    email: 'jaewonyu.dev@gmail.com'
+    email: 'jaewonyu.dev@gmail.com',
   },
   {
     name: 'WieeRd',
     description: '',
-    image: require('../assets/images/profile/WieeRd.png'),
-    email: ''
+    email: '',
   },
   {
     name: '라피스',
     description: '꾸준히 공부하고 성장하려 노력하는 학생 개발자입니다.',
-    image: require('../assets/images/profile/Lapis.png'),
-    email: 'lapis0875@gmail.com'
+    email: 'lapis0875@gmail.com',
   },
   {
     name: 'Nitro',
     description: '배움을 받고 배움을 공유하는 개발자가 되고 싶습니다.',
-    image: require('../assets/images/profile/Nitro.png'),
-    email: 'admin@nitrostudio.dev'
+    email: 'admin@nitrostudio.dev',
   },
   {
     name: '༺ৡۣۜ͜B̸1̷a̶c̵k̸A̸n̸g̶e̴1̸ৡۣۜ༻',
     description: 'Developer',
-    image: require('../assets/images/profile/Black_Angel.png'),
-    email: 'zeus8502@gmail.com'
+    email: 'zeus8502@gmail.com',
   },
   {
     name: 'F E Z',
     description: '뇌절온 중2 개발자',
-    image: require('../assets/images/profile/FEZ.png'),
-    email:'qnqn60360@gmail.com'
+    email: 'qnqn60360@gmail.com',
   },
   {
     name: '러리',
     description: '안녕하세요! 잡탕 러리입니다 :)  ',
-    image: require('../assets/images/profile/doralife.png'),
-    email: 'doralife12@gmail.com'
+    email: 'doralife12@gmail.com',
   },
   {
     name: 'Tentacles',
     description: '수학조아',
-    image: require('../assets/images/profile/Isomorphism.png'),
-    email: ''
+    email: '',
   },
   {
     name: '키네틱',
     description: '마신 커피만큼 코드를 뽑아냅니다.',
-    image: require('../assets/images/profile/Kinetic.png'),
-    email: 'aheui@kakao.com'
+    email: 'aheui@kakao.com',
   },
   {
     name: '중딩 프로그래머',
     description: '웹 개발에 관심이 많은 중학생 프로그래머입니다!',
-    image: require('../assets/images/profile/default_profile.png'),
-    email: ''
+    email: '',
   },
   {
     name: 'LEDAM',
     description: '파이썬은 빵과 태양',
-    image: require('../assets/images/profile/rodine.png'),
-    email: ''
+    email: '',
   },
   {
     name: '암드빠인 고양이',
     description: '프로그래밍에 관심이 많은 중학생 암고입니다',
-    image: require('../assets/images/profile/AMDCAT.png'),
-    email: 'june2006a@gmail.com'
+    email: 'june2006a@gmail.com',
   },
   {
     name: '키키',
     description: '파이썬을 사랑하는 학생 개발자입니다',
-    image: require('../assets/images/profile/kiki.png'),
-    email: 'devkiki7000@gmail.com'
-  }
+    email: 'devkiki7000@gmail.com',
+  },
 ];
